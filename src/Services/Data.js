bc9521601ddb0cfd343c5c5a19295ea923b0a649
Ryan <ryancanfield@me.com>
2018-01-09 17:41:52 -0800

    Add data

---
+++ src/Services/Data.js
@@ -0,0 +1,188 @@
+export const CLIENTS = [
+  {
+    title : 'eBay Enterprise',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/ebay-enterprise.png',
+    url   : ''
+  },
+  {
+    title : 'Blue Cross Blue Shield',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/bcbs.png',
+    url   : ''
+  },
+  {
+    title : 'Arizona State University',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/asu.png',
+    url   : 'http://pat.usmexpat.com'
+  },
+  {
+    title : 'University of Arizona',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/uoa.png',
+    url   : 'http://researchaz.org'
+  },
+  {
+    title : 'Northern Arizona University',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/nau.png',
+    url   : 'http://researchaz.org'
+  },
+  {
+    title : 'Hotelogical',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/hotelogical.png',
+    url   : 'http://hotelogical.com'
+  },
+  {
+    title : 'Hotels Ugogo',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/hotels-ugogo.png',
+    url   : 'http://hotelsugogo.com'
+  },
+  {
+    title : 'TruTankless',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/trutankless.png',
+    url   : 'http://trutankless.com'
+  },
+  {
+    title : 'BodeTree',
+    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/bodetree.png',
+    url   : 'http://bodetree.com'
+  },
+  // {
+  //   title : 'Beacon ID',
+  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/beacon-id.png',
+  //   url   : ''
+  // },
+  // {
+  //   title : 'Puppies.com',
+  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/puppies.png',
+  //   url   : 'http://puppies.com'
+  // },
+  // {
+  //   title : 'High Above',
+  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/highabove.png',
+  //   url   : 'http://highabove.net'
+  // },
+  // {
+  //   title : 'Carnival Cruiselines',
+  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/carnival.png',
+  //   url   : 'http://ocean.com'
+  // }
+];
+
+export const PROJECTS = [
+  {
+    title  : 'Chat Bubbles',
+    id     : 'GmMaVx'
+  },
+  {
+    title  : 'Expanding Hamburger Navigation Icon',
+    id     : 'pvewaX'
+  },
+  {
+    title  : 'Bottom Card Navigation',
+    id     : 'EagxGR'
+  },
+  {
+    title  : 'Dropdown Mini Menu',
+    id     : 'vymmZR'
+  },
+  {
+    title  : 'Playing Card Flop Animation',
+    id     : 'ulDvK'
+  },
+  {
+    title  : 'Sneaky Modal Effect',
+    id     : 'zwWOwj'
+  },
+  {
+    title  : 'Expanding Menu Effect',
+    id     : 'xgGdra'
+  },
+  {
+    title  : 'Knockout Text on Button',
+    id     : 'rmbdgg'
+  }
+];
+
+export const CARD_LINKS = [
+  {
+    title       : 'Shopify React Scripts',
+    description : 'Bespoke Shopify/React Projects in Minutes',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/shopify-react.png',
+    url         : 'https://medium.com/helpful-human/shopify-react-scripts-6e717791d7b4',
+    length      : '2 min read'
+  },
+  {
+    title       : 'Improving Teamwork through Knowledge Sharing',
+    description : 'Internal meetings for team unity',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/tobythealien.jpg',
+    url         : 'https://medium.com/helpful-human/improving-teamwork-through-knowledge-sharing-e3c6d53e6409',
+    length      : '3 min read'
+  },
+  {
+    title       : 'SVG icon sets in React with Rollup',
+    description : 'Follow up to "Embedded SVG icon sets and Reactjs"',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/svg-rollup.png',
+    url         : 'https://medium.com/helpful-human/svg-icon-sets-in-react-with-rollup-cd10be8206a5',
+    length      : '2 min read'
+  },
+  {
+    title       : 'Improving User Consideration in Development',
+    description : 'While web designers consider end users throughout the design process, developers can get caught up in implementation details and lose focus on why they are writing code in the first place — users.',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/user-consideration.png',
+    url         : 'https://medium.com/helpful-human/improving-user-consideration-in-development-604a4ddeb6dd',
+    length      : '4 min read'
+  },
+  {
+    title       : 'Creating a Custom, Maintainable React-Scripts Package',
+    description : 'When Facebook released Create React App, I was excited to be able to harness their knowledge of the build process in my applications.',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/react-scripts.png',
+    url         : 'https://medium.com/helpful-human/creating-a-custom-maintainable-react-scripts-package-db6d16501a94',
+    length      : '4 min read'
+  },
+  {
+    title       : 'Process & Method',
+    description : 'An adaptation from the speech I gave at Phoenix Design Week’s Pecha Kucha talks.',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/icons.png',
+    url         : 'https://medium.com/@Canfie1d/process-method-bddef9f5e47f',
+    length      : '6 min read'
+  },
+  {
+    title       : 'Embedded SVG icon sets and Reactjs',
+    description : 'How I implemented icons at Synapse Studios',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/sketch.png',
+    url         : 'https://medium.com/@Canfie1d/reactjs-and-embedded-svg-icons-1e6eed0dc16a',
+    length      : '4 min read'
+  },
+  {
+    title       : 'SMACSS/BEM edge case naming convention',
+    description : 'What do you do in cases where BEM methodology fails? What does the fallback naming convention look like?',
+    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/code.png',
+    url         : 'https://medium.com/@Canfie1d/smacss-bem-edge-case-naming-convention-73be902b1d30',
+    length      : '3 min read'
+  },
+];
+
+export const ELSEWHERE_ITEMS = [
+  {
+    title    : 'Email',
+    icon     : 'email',
+    url      : 'mailto:ryancanfield@me.com',
+    external : true
+  },
+  {
+    title    : 'Github',
+    icon     : 'github',
+    url      : 'http://www.github.com/canfie1d',
+    external : true
+  },
+  {
+    title    : 'Twitter',
+    icon     : 'twitter',
+    url      : 'http://www.twitter.com/canfie1d',
+    external : true
+  },
+  {
+    title    : 'LinkedIn',
+    icon     : 'linkedIn',
+    url      : 'http://www.linkedin.com/in/ryanmcanfield',
+    external : true
+  }
+];
