bc0f30df87f3d2082272ef72a1d9458f2153a37d
Ryan <ryancanfield@me.com>
2018-01-10 16:41:41 -0800

    Add new icons

---
+++ src/Routes/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Introduction from '../Components/Introduction';
+
+export default class NotFound extends React.Component {
+  render () {
+    return (
+      <main className='page'>
+        <Introduction title='Page Not Found.' subtitle="Sorry, but the page you are looking for does not exist." />
+      </main>
+    )
+  }
+}
