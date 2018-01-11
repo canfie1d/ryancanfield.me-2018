bcd458d06b1e3c8873db16f60aa21157c60d30c6
Ryan <ryancanfield@me.com>
2018-01-10 16:39:20 -0800

    Move layout from routes to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from 'react';
+import Header from './Components/Header';
+import { ScrollToTop } from './Services/ScrollToTop';
+
 import './App.styl';
 
 export default class App extends React.Component {
   render() {
     return (
       <div className="app">
-        {this.props.children}
+        <div className='l'>
+          <ScrollToTop />
+          <div className='l--rightColumn'>
+            <a className='visually-hidden' href="#scrollArea">Skip to content</a>
+            <Header />
+          </div>
+          <div className='l--leftColumn' id='scrollArea'>
+            {this.props.children}
+          </div>
+        </div>
       </div>
     );
   }
--- src/routes.js
+++ src/routes.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Route, Switch } from 'react-router';
 
-import Header from './Components/Header';
-
 // PAGES
-import Home from './Containers/Home';
+import About from './Containers/About';
 import Concepts from './Routes/Concepts';
 import Contact from './Routes/Contact';
-import NotFound from './Components/NotFound';
+import Projects from './Routes/Projects';
+import NotFound from './Routes/NotFound';
 
 export default (
-  <div className='l'>
-    <div className='l--leftColumn'>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route exact path='/concepts' component={Concepts} />
-        <Route exact path='/contact' component={Contact} />
-        <Route component={NotFound} />
-      </Switch>
-    </div>
-    <div className='l--rightColumn'>
-      <Header />
-    </div>
-  </div>
+  <Switch>
+    <Route exact path="/" component={About} />
+    <Route exact path='/concepts' component={Concepts} />
+    <Route exact path='/projects' component={Projects} />
+    <Route exact path='/contact' component={Contact} />
+    <Route component={NotFound} />
+  </Switch>
 );
