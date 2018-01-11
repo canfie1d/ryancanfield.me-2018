1598088790541a90a5dc24594e37d8e3389a1865
Ryan <ryancanfield@me.com>
2018-01-10 17:28:01 -0800

    Add contact content

--- src/Routes/Contact.js
+++ src/Routes/Contact.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import Icon from '../Components/Icon';
 import Introduction from '../Components/Introduction';
 import Footer from '../Components/Footer';
 
@@ -8,10 +9,35 @@ export default class Landing extends React.Component {
       <main className='page'>
         <Introduction title="Contact Me." subtitle="You can reach to out me in several ways." />
         <div className='content'>
-          <a href="mailto:ryancanfield@me.com?subject=Hello from ryancanfield.me" >Email</a>
-          <a href="http://www.twitter.com/canfie1d">Twitter</a>
-          <a href="http://www.github.com/canfie1d">Github</a>
-          <a href="http://www.linkedin.com/in/ryanmcanfield">LinkedIn</a>
+          <div className='contact-card'>
+            <h3 className='h3'>Would you like to work together or simply chat about a project? Don't hesitate to contact me.</h3>
+            <ul className='contact-card__list'>
+              <li className='contact-card__item'>
+                <a className='a' href="mailto:ryancanfield@me.com?subject=Hello from ryancanfield.me" >
+                  <Icon color='primary' size='x-large' icon='email'/>
+                  <span className='visually-hidden'>Email</span>
+                </a>
+              </li>
+              <li className='contact-card__item'>
+                <a className='a' href="http://www.twitter.com/canfie1d">
+                  <Icon color='primary' size='x-large' icon='twitter'/>
+                  <span className='visually-hidden'>Twitter</span>
+                </a>
+              </li>
+              <li className='contact-card__item'>
+                <a className='a' href="http://www.github.com/canfie1d">
+                  <Icon color='primary' size='x-large' icon='github'/>
+                  <span className='visually-hidden'>Github</span>
+                </a>
+              </li>
+              <li className='contact-card__item'>
+                <a className='a' href="http://www.linkedin.com/in/ryanmcanfield">
+                  <Icon color='primary' size='x-large' icon='linkedin'/>
+                  <span className='visually-hidden'>LinkedIn</span>
+                </a>
+              </li>
+            </ul>
+          </div>
         </div>
         <Footer />
       </main>
