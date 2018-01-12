9f84d0821de59dc5c6ca56b26d6060f3e95f0888
Ryan <ryancanfield@me.com>
2018-01-12 14:33:59 -0800

    Add project data

--- src/Routes/Projects.js
+++ src/Routes/Projects.js
@@ -2,87 +2,83 @@ import React from 'react';
 import Introduction from '../Components/Introduction';
 import Icon from '../Components/Icon';
 import Footer from '../Components/Footer';
+import AnimatedWaypoint from '../Containers/AnimatedWaypoint';
+import { CLIENTS, PROJECTS } from '../Services/Data';
 
 export default class Projects extends React.Component {
+
+  renderProjectList() {
+    const projectList = [];
+
+    for (let i = 0; i < PROJECTS.length; i++) {
+      projectList.push(
+        <li key={PROJECTS[i].title}>
+          <a className='a' href={`http://www.codepen.io/canfie1d/full/${PROJECTS[i].id}`}>{PROJECTS[i].title}</a>
+        </li>
+      );
+    }
+
+    return <ul>{projectList}</ul>;
+  }
+
+  renderClientList() {
+    const clientList = [];
+
+    for (let i = 0; i < CLIENTS.length; i++) {
+      if (CLIENTS[i].url) {
+        clientList.push(
+          <li key={CLIENTS[i].title} className='client__item client__item--hasLink'>
+            <a href={CLIENTS[i].url} className='a client__link'>{CLIENTS[i].title}</a>
+          </li>
+        );
+      } else {
+        clientList.push(
+          <li key={CLIENTS[i].title} className='client__item'>
+            {CLIENTS[i].title}
+          </li>
+        );
+      }
+    }
+
+    return <ul className='client__list'>{clientList}</ul>;
+  }
+
   render() {
     return (
       <main className='page'>
         <Introduction title="My Work." subtitle="A selection of projects, demos &amp;&nbsp;clients." />
         <div className='content'>
-        <div className='content__split'>
-            <div className='content__split__column'>
+          <div className='content__split'>
+            <AnimatedWaypoint className='content__split__column'>
+              <div className='hr' />
               <h3 className='h3'>Companies I've worked with</h3>
-              <ul className='client__list'>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://pat.ocean.com' className='a client__link'>
-                    Carnival Cruiselines
-                  </a>
-                </li>
-                <li className='client__item'>eBay Enterprise</li>
-                <li className='client__item'>Blue Cross Blue Shield</li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://pat.usmexpat.com' className='a client__link'>
-                    Arizona State University
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://researchaz.org' className='a client__link'>
-                    University of Arizona
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://researchaz.org' className='a client__link'>
-                    Northern Arizona University
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://hotelogical.com' className='a client__link'>
-                    Hotelogical
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://hotelsugogo.com' className='a client__link'>
-                    Hotels Ugogo
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://trutankless.com' className='a client__link'>
-                    TruTankless
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://bodetree.com' className='a client__link'>
-                    BodeTree
-                  </a>
-                </li>
-                <li className='client__item client__item--hasLink'>
-                  <a href='http://synapsestudios.com' className='a client__link'>
-                    Synapse Studios
-                  </a>
-                </li>
-                <li className='client__item'>Puppies.com</li>
-                <li className='client__item'>High Above</li>
-              </ul>
-            </div>
-            <div className='content__split__column'>
+              {this.renderClientList()}
+            </AnimatedWaypoint>
+            <AnimatedWaypoint className='content__split__column'>
+              <div className='hr' />
               <h3 className='h3'>Open Source Projects</h3>
               <ul>
                 <li>
                   <h4 className='h4'>Complete React Scripts</h4>
-                  <a className='a' href='http://www.github.com/canfie1d/complete-react-scripts'><Icon icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
-                  <a className='a' href='https://www.npmjs.com/package/complete-react-scripts'><Icon icon='email' size='x-large' /><span className='visually-hidden'>NPM</span></a>
+                  <a className='a' href='http://www.github.com/canfie1d/complete-react-scripts'><Icon color='primary' icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
+                  <a className='a' href='https://www.npmjs.com/package/complete-react-scripts'><Icon color='primary' icon='email' size='x-large' /><span className='visually-hidden'>NPM</span></a>
                 </li>
                 <li>
                   <h4 className='h4'>Shopify React Scripts</h4>
-                  <a className='a' href='http://www.github.com/canfie1d/shopify-react-scripts'><Icon icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
-                  <a className='a' href='https://www.npmjs.com/package/shopify-react-scripts'><Icon icon='email' size='x-large' /><span className='visually-hidden'>NPM</span></a>
+                  <a className='a' href='http://www.github.com/canfie1d/shopify-react-scripts'><Icon color='primary' icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
+                  <a className='a' href='https://www.npmjs.com/package/shopify-react-scripts'><Icon color='primary' icon='email' size='x-large' /><span className='visually-hidden'>NPM</span></a>
                 </li>
                 <li>
                   <h4 className='h4'>Slack Statusbot</h4>
-                  <a className='a' href='https://github.com/canfie1d/statusbot'><Icon icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
+                  <a className='a' href='https://github.com/canfie1d/statusbot'><Icon color='primary' icon='github' size='x-large' /><span className='visually-hidden'>Github</span></a>
                 </li>
               </ul>
-            </div>
+            </AnimatedWaypoint>
+            <AnimatedWaypoint className='content__split__column content__split__column--full'>
+              <div className='hr' />
+              <h3 className='h3'>Code Snippets &amp; Demos</h3>
+              {this.renderProjectList()}
+            </AnimatedWaypoint>
           </div>
         </div>
         <Footer />
--- src/Services/Data.js
+++ src/Services/Data.js
@@ -1,188 +1,145 @@
 export const CLIENTS = [
   {
-    title : 'eBay Enterprise',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/ebay-enterprise.png',
-    url   : ''
+    title: 'Carnival Cruiselines',
+    url: 'http://ocean.com'
   },
   {
-    title : 'Blue Cross Blue Shield',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/bcbs.png',
-    url   : ''
+    title: 'eBay Enterprise'
   },
   {
-    title : 'Arizona State University',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/asu.png',
-    url   : 'http://pat.usmexpat.com'
+    title: 'Blue Cross Blue Shield'
   },
   {
-    title : 'University of Arizona',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/uoa.png',
-    url   : 'http://researchaz.org'
+    title: 'Arizona State University',
+    url: 'http://pat.usmexpat.com'
   },
   {
-    title : 'Northern Arizona University',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/nau.png',
-    url   : 'http://researchaz.org'
+    title: 'University of Arizona',
+    url: 'http://researchaz.org'
   },
   {
-    title : 'Hotelogical',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/hotelogical.png',
-    url   : 'http://hotelogical.com'
+    title: 'Northern Arizona University',
+    url: 'http://researchaz.org'
   },
   {
-    title : 'Hotels Ugogo',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/hotels-ugogo.png',
-    url   : 'http://hotelsugogo.com'
+    title: 'Hotelogical',
+    url: 'http://hotelogical.com'
   },
   {
-    title : 'TruTankless',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/trutankless.png',
-    url   : 'http://trutankless.com'
+    title: 'Hotels Ugogo',
+    url: 'http://hotelsugogo.com'
   },
   {
-    title : 'BodeTree',
-    image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/bodetree.png',
-    url   : 'http://bodetree.com'
+    title: 'TruTankless',
+    url: 'http://trutankless.com'
   },
-  // {
-  //   title : 'Beacon ID',
-  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/beacon-id.png',
-  //   url   : ''
-  // },
-  // {
-  //   title : 'Puppies.com',
-  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/puppies.png',
-  //   url   : 'http://puppies.com'
-  // },
-  // {
-  //   title : 'High Above',
-  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/highabove.png',
-  //   url   : 'http://highabove.net'
-  // },
-  // {
-  //   title : 'Carnival Cruiselines',
-  //   image : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/clients/carnival.png',
-  //   url   : 'http://ocean.com'
-  // }
+  {
+    title: 'BodeTree',
+    url: 'http://bodetree.com'
+  },
+  {
+    title: 'Beacon ID'
+  },
+  {
+    title: 'Puppies.com',
+    url: 'http://puppies.com'
+  },
+  {
+    title: 'High Above',
+    url: 'http://highabove.net'
+  }
 ];
 
 export const PROJECTS = [
   {
-    title  : 'Chat Bubbles',
-    id     : 'GmMaVx'
+    title: 'Chat Bubbles',
+    id: 'GmMaVx'
   },
   {
-    title  : 'Expanding Hamburger Navigation Icon',
-    id     : 'pvewaX'
+    title: 'Expanding Hamburger Navigation Icon',
+    id: 'pvewaX'
   },
   {
-    title  : 'Bottom Card Navigation',
-    id     : 'EagxGR'
+    title: 'Bottom Card Navigation',
+    id: 'EagxGR'
   },
   {
-    title  : 'Dropdown Mini Menu',
-    id     : 'vymmZR'
+    title: 'Dropdown Mini Menu',
+    id: 'vymmZR'
   },
   {
-    title  : 'Playing Card Flop Animation',
-    id     : 'ulDvK'
+    title: 'Playing Card Flop Animation',
+    id: 'ulDvK'
   },
   {
-    title  : 'Sneaky Modal Effect',
-    id     : 'zwWOwj'
+    title: 'Sneaky Modal Effect',
+    id: 'zwWOwj'
   },
   {
-    title  : 'Expanding Menu Effect',
-    id     : 'xgGdra'
+    title: 'Expanding Menu Effect',
+    id: 'xgGdra'
   },
   {
-    title  : 'Knockout Text on Button',
-    id     : 'rmbdgg'
+    title: 'Knockout Text on Button',
+    id: 'rmbdgg'
   }
 ];
 
 export const CARD_LINKS = [
   {
-    title       : 'Shopify React Scripts',
-    description : 'Bespoke Shopify/React Projects in Minutes',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/shopify-react.png',
-    url         : 'https://medium.com/helpful-human/shopify-react-scripts-6e717791d7b4',
-    length      : '2 min read'
+    title: 'Shopify React Scripts',
+    description: 'Bespoke Shopify/React Projects in Minutes',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/shopify-react.png',
+    url: 'https://medium.com/helpful-human/shopify-react-scripts-6e717791d7b4',
+    length: '2 min read'
   },
   {
-    title       : 'Improving Teamwork through Knowledge Sharing',
-    description : 'Internal meetings for team unity',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/tobythealien.jpg',
-    url         : 'https://medium.com/helpful-human/improving-teamwork-through-knowledge-sharing-e3c6d53e6409',
-    length      : '3 min read'
+    title: 'Improving Teamwork through Knowledge Sharing',
+    description: 'Internal meetings for team unity',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/tobythealien.jpg',
+    url: 'https://medium.com/helpful-human/improving-teamwork-through-knowledge-sharing-e3c6d53e6409',
+    length: '3 min read'
   },
   {
-    title       : 'SVG icon sets in React with Rollup',
-    description : 'Follow up to "Embedded SVG icon sets and Reactjs"',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/svg-rollup.png',
-    url         : 'https://medium.com/helpful-human/svg-icon-sets-in-react-with-rollup-cd10be8206a5',
-    length      : '2 min read'
+    title: 'SVG icon sets in React with Rollup',
+    description: 'Follow up to "Embedded SVG icon sets and Reactjs"',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/svg-rollup.png',
+    url: 'https://medium.com/helpful-human/svg-icon-sets-in-react-with-rollup-cd10be8206a5',
+    length: '2 min read'
   },
   {
-    title       : 'Improving User Consideration in Development',
-    description : 'While web designers consider end users throughout the design process, developers can get caught up in implementation details and lose focus on why they are writing code in the first place — users.',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/user-consideration.png',
-    url         : 'https://medium.com/helpful-human/improving-user-consideration-in-development-604a4ddeb6dd',
-    length      : '4 min read'
+    title: 'Improving User Consideration in Development',
+    description: 'While web designers consider end users throughout the design process, developers can get caught up in implementation details and lose focus on why they are writing code in the first place — users.',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/user-consideration.png',
+    url: 'https://medium.com/helpful-human/improving-user-consideration-in-development-604a4ddeb6dd',
+    length: '4 min read'
   },
   {
-    title       : 'Creating a Custom, Maintainable React-Scripts Package',
-    description : 'When Facebook released Create React App, I was excited to be able to harness their knowledge of the build process in my applications.',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/react-scripts.png',
-    url         : 'https://medium.com/helpful-human/creating-a-custom-maintainable-react-scripts-package-db6d16501a94',
-    length      : '4 min read'
+    title: 'Creating a Custom, Maintainable React-Scripts Package',
+    description: 'When Facebook released Create React App, I was excited to be able to harness their knowledge of the build process in my applications.',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/react-scripts.png',
+    url: 'https://medium.com/helpful-human/creating-a-custom-maintainable-react-scripts-package-db6d16501a94',
+    length: '4 min read'
   },
   {
-    title       : 'Process & Method',
-    description : 'An adaptation from the speech I gave at Phoenix Design Week’s Pecha Kucha talks.',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/icons.png',
-    url         : 'https://medium.com/@Canfie1d/process-method-bddef9f5e47f',
-    length      : '6 min read'
+    title: 'Process & Method',
+    description: 'An adaptation from the speech I gave at Phoenix Design Week’s Pecha Kucha talks.',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/icons.png',
+    url: 'https://medium.com/@Canfie1d/process-method-bddef9f5e47f',
+    length: '6 min read'
   },
   {
-    title       : 'Embedded SVG icon sets and Reactjs',
-    description : 'How I implemented icons at Synapse Studios',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/sketch.png',
-    url         : 'https://medium.com/@Canfie1d/reactjs-and-embedded-svg-icons-1e6eed0dc16a',
-    length      : '4 min read'
+    title: 'Embedded SVG icon sets and Reactjs',
+    description: 'How I implemented icons at Synapse Studios',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/sketch.png',
+    url: 'https://medium.com/@Canfie1d/reactjs-and-embedded-svg-icons-1e6eed0dc16a',
+    length: '4 min read'
   },
   {
-    title       : 'SMACSS/BEM edge case naming convention',
-    description : 'What do you do in cases where BEM methodology fails? What does the fallback naming convention look like?',
-    imageUrl    : 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/code.png',
-    url         : 'https://medium.com/@Canfie1d/smacss-bem-edge-case-naming-convention-73be902b1d30',
-    length      : '3 min read'
+    title: 'SMACSS/BEM edge case naming convention',
+    description: 'What do you do in cases where BEM methodology fails? What does the fallback naming convention look like?',
+    imageUrl: 'https://s3-us-west-2.amazonaws.com/ryancanfield.me-images/Link+Images/code.png',
+    url: 'https://medium.com/@Canfie1d/smacss-bem-edge-case-naming-convention-73be902b1d30',
+    length: '3 min read'
   },
-];
-
-export const ELSEWHERE_ITEMS = [
-  {
-    title    : 'Email',
-    icon     : 'email',
-    url      : 'mailto:ryancanfield@me.com',
-    external : true
-  },
-  {
-    title    : 'Github',
-    icon     : 'github',
-    url      : 'http://www.github.com/canfie1d',
-    external : true
-  },
-  {
-    title    : 'Twitter',
-    icon     : 'twitter',
-    url      : 'http://www.twitter.com/canfie1d',
-    external : true
-  },
-  {
-    title    : 'LinkedIn',
-    icon     : 'linkedIn',
-    url      : 'http://www.linkedin.com/in/ryanmcanfield',
-    external : true
-  }
 ];
