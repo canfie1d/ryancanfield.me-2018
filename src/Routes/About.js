3edcb822b4d5bcb910bba166334295ed8ce1e188
Ryan <ryancanfield@me.com>
2018-01-22 17:57:37 -0800

    Add new content

--- src/Routes/About.js
+++ src/Routes/About.js
@@ -14,16 +14,31 @@ export default class About extends React.Component {
               <div className='content__split__column'>
                 <div className='hr' />
                 <h3 className='h3'>Work &amp; Education</h3>
-                <p className='p'>In my last position, I specialized in user interface design &amp; front-end development at <a href='http://helpfulhuman.com' className='a'>Helpful Human</a>. Previously, I was a UI engineer at <a href='http://www.synapsestudios.com' className='a'>Synapse Studios</a> and prior to that, I was a designer for the <a href='http://www.azstatefair.com' className='a'>Arizona State Fair</a> in Phoenix, AZ.</p>
-                <p className='p'>In 2012, I earned my Bachelors of Fine Arts in Graphic Design from Collins College. Prior to that, in 2010, I earned an Associates in Information Technology from Bryant &amp; Stratton College in New York. In addition to my traditional graphic design education, I'm extremely comfortable with HTML, CSS(SCSS &amp; Stylus) &amp; Javascript.</p>
+                <p className='p'>In 2009, I earned an Associates in Information Technology as a member of the National Honor Society. In May 2012, I earned a Bachelor of Fine Art in Graphic Design with Honors/Cum Laude &amp; as the Salutatorian.</p>
+                <p className='p'>Shortly thereafter, I began my career as a designer for the <a className='a' href='http://www.azstatefair.com'>Arizona State Fair</a> in Phoenix, AZ before focusing my career on web design and development. By October 2012, I was offered a position as a UI engineer at <a href='http://www.synapsestudios.com' className='a'>Synapse Studios</a>. A position I held until my move to Seattle, WA in 2016.</p>
+                <p className='p'>Following the move to Seattle, I began a new position at <a className='a' href='http://helpfulhuman.com'>Helpful Human</a> where I specialized in user interface design &amp; front-end development.</p>
               </div>
             </AnimatedWaypoint>
             <AnimatedWaypoint>
               <div className='content__split__column'>
                 <div className='hr' />
                 <h3 className='h3'>Expertise</h3>
-                <p className='p'>I use an assortment of tools such as Sketch, Principle, Marvel &amp; inVision to design and prototype products.</p>
-                <p className='p'>I am very accustomed to using agile development methodology including daily stand-up, sprint based development cycles, and iterative and incremental development. I am comfortable with complex git workflows as well as kanban style issue tracking services.</p>
+                <p className='p'>While designing for digital products, I use an assortment of tools such as <a className='a' href="https://www.sketchapp.com/">Sketch</a>, Principle, Marvel &amp; inVision to design and prototype. I am always on the hunt for tools that will allow me to design more iteratively and effectively.</p>
+                <p className='p'>In addition to my traditional graphic design education, I'm extremely comfortable with HTML, CSS(SCSS &amp; Stylus) &amp; Javascript. In development I specialize in dynamic user interfaces built with javascript (<a className='a' href='http://www.reactjs.org'>Reactjs</a> in particular). I strive for maintainable, extensible code that is accessible and user-centric.</p>
+                <p className='p'>I am accustomed to an agile development flow including daily stand-up, sprint based development, and iterative &amp; incremental development cycles. I am comfortable with complex git workflows as well as kanban style issue tracking services.</p>
+                <p className='p'>I work well on a team and can brainstorm and problem solve sticking points on the fly; especially in areas where design and development meet. Because I can speak both languages, I am an excellent liason between design and development teams.</p>
+              </div>
+            </AnimatedWaypoint>
+            <AnimatedWaypoint>
+              <div className='content__split__column'>
+                <div className='hr' />
+                <h3 className='h3'>Professional Acheivements &amp; Initiatives</h3>
+                <p className='p'>Spoke about personal design &amp; development methodology to an audience of ~200 at Phoenix Design Week in 2016.</p>
+                <p className='p'>Established a weekly team training session in which one team member would research and present on a topic of interest in order to expand domain knowledge and team knowledge sharing capabilities.</p>
+                <p className='p'>Technical lead for a 3 person team developing an application in collaboration with Shopify.</p>
+                <p className='p'>Developed and maintained application frameworks, user interface component libraries, and knowledge bases in multiple previous positions.</p>
+                <p className='p'>Introduced the importance and advocated for the inclusion of accessibility and building accessible digital products in multiple previous positions.</p>
+                <p className='p'>Introduced company-wide coding standards to assist in developing highly readable &amp; maintainable source code.</p>
               </div>
             </AnimatedWaypoint>
           </div>
